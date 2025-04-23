4ee4ff0fce20337ab9b114f9d243fca879600748
Rabi Bhujel <screwup39@gmail.com>
2025-04-23 03:36:03 +0000

    Pending changes exported from your codespace

---
+++ components/Entry.jsx
@@ -0,0 +1,49 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'; // import the actual icon
+
+function Entry(props) {
+  return (
+    /*
+    <section>
+      <nav><img src="../images/mountFuji.png" /></nav>
+      <article>
+        <h2>
+          <FontAwesomeIcon icon={faLocationDot} /> Japan
+        </h2>
+        <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">
+          View on Google Maps
+        </a>
+        <h1> Mount Fuji</h1>
+        <h3>12 Jan, 2021 - 24 Jan, 2021</h3>
+        <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+      </article>
+
+    </section>
+    */
+
+    <article className="journal-entry">
+      <div className="main-image-container">
+        <img
+          className="main-image"
+          src={props.image}
+          alt={props.alt}
+        />
+      </div>
+      <div className="info-container">
+        <img
+          className="marker"
+          src={props.marker}
+          alt="map marker icon"
+        />
+        <span className="country">{props.country}</span>
+        <a href={props.googleMapLink}> View on Google Maps</a>
+        <h2 className="entry-title">{props.title}</h2>
+        <p className="trip-dates">{props.dates}</p>
+        <p className="entry-text">{props.text}</p>
+      </div>
+
+    </article >
+  )
+}
+
+export default Entry;
---
+++ components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <header className="head">
+            <img src="../images/globe.jpg" alt="globe" />
+            <h1>My Travel Journal</h1>
+        </header>
+
+
+    )
+}
+export default Header;
