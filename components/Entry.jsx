205b25131e82060b1760b217876a5708829d9965
vzl-rbi <screwup39@gmail.com>
2025-05-11 12:03:06 +0545

    added and modified

--- components/Entry.jsx
+++ components/Entry.jsx
@@ -1,28 +1,28 @@
-
- function Entry(props) {
+function Entry(props) {
   return (
-      <article className="journal-entry">
-          <div className="main-image-container">
-              <img 
-                  className="main-image"
-                  src={props.entry.img.src}
-                  alt={props.entry.img.alt}
-              />
-          </div>
-          <div className="info-container">
-              <img 
-                  className="marker"
-                  src="../images/marker.png" 
-                  alt="map marker icon"
-              />
-              <span className="country">{props.entry.country}</span>
-              <a href={props.entry.googleMapsLink} target="_blank">View on Google Maps</a>
-              <h2 className="entry-title">{props.entry.title}</h2>
-              <p className="trip-dates">{props.entry.dates}</p>
-              <p className="entry-text">{props.entry.text}</p>
-          </div>
-          
-      </article>
-  )
+    <article className="journal-entry">
+      <div className="main-image-container">
+        <img
+          className="main-image"
+          src={props.entry.img.src}
+          alt={props.entry.img.alt}
+        />
+      </div>
+      <div className="info-container">
+        <img
+          className="marker"
+          src="../images/marker.png"
+          alt="map marker icon"
+        />
+        <span className="country">{props.entry.country}</span>
+        <a href={props.entry.googleMapsLink} target="_blank">
+          View on Google Maps
+        </a>
+        <h2 className="entry-title">{props.entry.title}</h2>
+        <p className="trip-dates">{props.entry.dates}</p>
+        <p className="entry-text">{props.entry.text}</p>
+      </div>
+    </article>
+  );
 }
 export default Entry;
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import Header from '../components/Header';
-import Entry from '../components/Entry';
-import data from './data';
+import Header from "../components/Header";
+import Entry from "../components/Entry";
+import data from "./data";
 function App() {
-    const entryData = data.map((entry) =>
-        <Entry
-        key={entry.id}
-        entry={entry} //alternative method is //{...entry} spread operator // 
-        /*this method make in Entry.jsx like this 
+  const entryData = data.map((entry) => (
+    <Entry
+      key={entry.id}
+      entry={entry} //alternative method is //{...entry} spread operator //
+      /*this method make in Entry.jsx like this 
         className="main-image"
         src={props.img.src}
         alt={props.img.alt} 
@@ -15,34 +15,14 @@ function App() {
         src={props.entry.img.src}
         alt={props.entry.img.alt}
         */
-        
-        />
-            
-    );
-    //const dataList = data.map((listData, index) =>
-        //<props
-            //key={index} //or Alternatively, key={listData.id} use this insted of index and remove index from map function
-           /* marker={listData.marker}
-            country={listData.country}
-            googleMapLink={listData.googleMapLink}
-            title={listData.title}
-            dates={listData.dates}
-            text={listData.text}
-            image={listData.img.src}
-            alt={listData.alt}
-           */
-       // />
-    //);
-    
-    return (
-        <>
-        <Header />
-        <main className="container">
-            {entryData}
-        </main>
-        </>
-        
+    />
+  ));
 
-    )
+  return (
+    <>
+      <Header />
+      <main className="container">{entryData}</main>
+    </>
+  );
 }
 export default App;
